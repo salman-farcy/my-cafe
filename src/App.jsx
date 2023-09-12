e0ebb6622d56ff3d174fd8f135dbbde7426ed9c2
salman-farcy <salmanfarcy253@gmail.com>
2023-09-12 13:32:51 +0600

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-4xl font-bold text-center bg-red-600 text-amber-300'>this is my furst smal simple react project</h1>
+      
+    </>
+  )
+}
+
+export default App
